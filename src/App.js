9ff9a77d0214da9c1c820dfc523f039264ab89f9
hellovietduc <hellovietduc21@gmail.com>
2020-09-27 17:18:15 +0700

    add react router

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
+import SharePage from './pages/Share';
+import CodePage from './pages/Code';
+
+function App() {
+  return (
+    <Router>
+      <div className="text-gray-700 body-font">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+          <Route exact path="/share">
+            <SharePage />
+          </Route>
+          <Route path="/:hash">
+            <CodePage />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,23 +3,23 @@ import React from 'react';
 function Header() {
   return (
     <header className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <button className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <a href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
         <span className="ml-3 text-xl">Code Share</span>
-      </button>
+      </a>
       <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
         <a href="https://github.com/hellovietduc/vscode-code-share" className="mr-5 hover:text-gray-900" rel="noopener noreferrer" target="_blank">VSCode extension</a>
       </nav>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <a href="/about" className="mr-5 hover:text-gray-900">About</a>
       </nav>
-      <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Create
-        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+      <a href="/share" className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">New share
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
         </svg>
-      </button>
+      </a>
     </header>
   );
 }
