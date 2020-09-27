53d771d640c89e1a277e1f1515ac5f4965353cb6
hellovietduc <hellovietduc21@gmail.com>
2020-09-27 17:17:57 +0700

    add home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <section>
+      <div className="text-center mt-24">
+        <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Instantly share your code with the world</h1>
+        <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Install the extension and follow these 4 simple steps</p>
+      </div>
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-green-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+          <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div className="flex-shrink-0 w-24 h-24 bg-green-100 text-green-500 rounded-full inline-flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-12 h-12" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+              </svg>
+            </div>
+            <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">Select the code you want to share</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-green-500 text-white relative z-10 title-font font-medium text-sm">2</div>
+          <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div className="flex-shrink-0 w-24 h-24 bg-green-100 text-green-500 rounded-full inline-flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-12 h-12" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
+            </div>
+            <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">Open Command Palette</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-green-500 text-white relative z-10 title-font font-medium text-sm">3</div>
+          <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div className="flex-shrink-0 w-24 h-24 bg-green-100 text-green-500 rounded-full inline-flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-12 h-12" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+              </svg>
+            </div>
+            <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">Type <code className="ml-1">Share Selected Code</code></h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+          <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-green-500 text-white relative z-10 title-font font-medium text-sm">4</div>
+          <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div className="flex-shrink-0 w-24 h-24 bg-green-100 text-green-500 rounded-full inline-flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-12 h-12" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+              </svg>
+            </div>
+            <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">Enjoy awesomeness!</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
+}
+
+export default HomePage;
