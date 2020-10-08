086c83253a5c09f1d9357e0260c5ca40ea945ec4
hellovietduc <hellovietduc21@gmail.com>
2020-10-08 11:47:54 +0700

    add loading screen code page

--- src/pages/Code/index.js
+++ src/pages/Code/index.js
@@ -7,7 +7,7 @@ import { getCode } from '../../utils/request';
 
 function CodePage() {
   const { pathname } = useLocation();
-  const [codeString, setCodeString] = useState(null);
+  const [codeString, setCodeString] = useState();
 
   useEffect(() => {
     const fetchCode = async () => {
@@ -19,20 +19,24 @@ function CodePage() {
 
   return (
     <section className="container px-5 py-24 mx-auto relative">
-      {
-        codeString === null
-          ? (
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">Code Not Found</p>
-          )
-          : (
-            <div className="lg:w-1/2 md:w-2/3 mx-auto">
-              <div className="flex flex-wrap justify-center">
-                <ReadOnlyInput value={window.location.href} />
-                <CopyBtn copyValue={window.location.href} />
+      {(() => {
+        switch (codeString) {
+          case undefined:
+            return <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">Loading</p>;
+          case null:
+            return <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">Code Not Found</p>;
+          default:
+            return (
+              <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <div className="flex flex-wrap justify-center">
+                  <ReadOnlyInput value={window.location.href} />
+                  <CopyBtn copyValue={window.location.href} />
+                </div>
+                <CodeArea code={codeString.trim()} />
               </div>
-              <CodeArea code={codeString.trim()} />
-            </div>
-          )
+            );
+        }
+      })()
       }
     </section>
   );
